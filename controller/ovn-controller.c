3bbf8d506c194e80268f0780b30a2c7c4b6f852e
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-15 10:16:44 -0400

    spelling: triggering
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- controller/ovn-controller.c
+++ controller/ovn-controller.c
@@ -5636,7 +5636,7 @@ main(int argc, char *argv[])
                     * try to run the engine so that we can handle any
                     * incremental changes that don't require a recompute.
                     * If a recompute is required, the engine will cancel,
-                    * triggerring a full run in the next iteration.
+                    * triggering a full run in the next iteration.
                     */
                     /* 2. ofctrl_has_backlog */
                     /* When there are in-flight messages pending to
